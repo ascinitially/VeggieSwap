6abe571d17705cdb612b40f09adb66d0103d2f5e
Johdi- Ann Moulton <45779037+jodubb@users.noreply.github.com>
2019-07-08 18:15:08 -0400

    Add files via upload
    latest log in and registration pages

---
+++ src/components/Grid/index.jsx
@@ -0,0 +1,185 @@
+﻿import React, { Component } from "react";
+// import classnames from "classnames";
+import GridComponent from "./GridComponent";
+import WarningMessage from "../WarningMessage";
+import GreyBox from "../../images/GreyBox.svg";
+// import styles from "./grid.module.css";
+import CONSTANTS from "../../constants";
+import backgrdimg from "./backgrd.jpg";
+import logo from "./veggieswap.png";
+import veggiemain from "./veggiemain.jpg";
+import veggiehand from "./handwithveggies.jpg";
+import carrotswoman from "./womanwithcarrots.jpg";
+import { Slide } from 'react-slideshow-image';
+
+
+// slideshow properties
+const slideImages = [
+  veggiemain,
+  veggiehand,
+  carrotswoman
+];
+
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true,
+ 
+  onChange: (oldIndex, newIndex) => {
+    console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+  }
+
+}
+
+const slideStyle = {
+  height:200
+}
+const Slideshow = () => {
+    return (
+      <Slide {...properties}>
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[0]})`,
+                        'height': 500,
+                        'background-size': 'cover',
+                        'background-repeat': 'no-repeat',
+                      
+        }}>
+            <span>Slide 1</span>
+          </div>
+        </div>
+        <div className="each-slide" style={slideStyle}>
+          <div style={{'backgroundImage': `url(${slideImages[1]})`,
+                      'height':500,
+                      'background-repeat': 'no-repeat',
+                     
+                      'background-size': 'cover',
+                      
+        }}>
+            <span>Slide 2</span>
+          </div>
+        </div>
+        <div className="each-slide" style={slideStyle}>
+          <div style={{'backgroundImage': `url(${slideImages[2]})`,
+                      'height':500,
+                      'background-size': 'cover',
+                      'background-repeat': 'no-repeat',
+                      'object-fit': 'fill',
+        }}>
+            <span>Slide 3</span>
+          </div>
+        </div>
+      </Slide>
+    )
+}
+
+export default class Grid extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gridTextAssets: [{ description: "", header: "", id: 0 }],
+      WarningMessageOpen: false,
+      WarningMessageText: ""
+    };
+
+    this.handleWarningClose = this.handleWarningClose.bind(this);
+  }
+
+  // Get the text sample data from the back end
+  componentDidMount() {
+    fetch(CONSTANTS.ENDPOINT.GRID)
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(result => this.setState({ gridTextAssets: result }))
+      .catch(error =>
+        this.setState({
+          WarningMessageOpen: true,
+          WarningMessageText: `Request to get grid text failed: ${error}`
+        })
+      );
+  }
+
+  handleWarningClose() {
+    this.setState({
+      WarningMessageOpen: false,
+      WarningMessageText: ""
+    });
+  }
+
+  render() {
+    const {
+      gridTextAssets,
+      WarningMessageOpen,
+      WarningMessageText
+    } = this.state;
+    return (
+      <main id="mainContent">
+        <div className= "text-center" style = {{
+            backgroundImage: `url(${logo})`,
+            backgroundRepeat: "no-repeat",
+            padding: 50,
+            margin: 20,
+            
+          }}>
+
+        </div>
+  
+
+
+  
+            <Slideshow>
+
+            </Slideshow>
+            
+       
+
+
+
+           
+
+
+
+          
+          
+       
+         
+         
+
+          
+       
+
+        <div className="container" style= {{
+          backgroundImage: `url(${backgrdimg})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat"
+        }}>
+          <div className="row justify-content-center py-5">
+            <h1>Bootstrap Grid Template</h1>
+          </div>
+
+          <div className="row justify-content-around text-center pb-5">
+            {gridTextAssets.map(textAssets => (
+              <GridComponent
+                key={textAssets.id}
+                header={textAssets.title}
+                description={textAssets.shortDescription}
+                image={GreyBox}
+              />
+            ))}
+          </div>
+        </div>
+        <WarningMessage
+          open={WarningMessageOpen}
+          text={WarningMessageText}
+          onWarningClose={this.handleWarningClose}
+        />
+      </main>
+    );
+  }
+}
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,47 @@
+﻿import React from "react";
+import { Link } from "react-router-dom";
+import "../../components/style/css.css";
+
+//TODO Web Template Studio: Add a new link in the NavBar for your page here.
+// A skip link is included as an accessibility best practice. For more information visit https://www.w3.org/WAI/WCAG21/Techniques/general/G1.
+export default function NavBar() {
+  return (
+    
+      <div class="w3-top">
+        <div class="w3-row w3-large w3-black" style = {styles.navBar}>
+
+            <div class="w3-col s3">
+                <Link to ="#" class="w3-button w3-block" style = {styles.linkStyles}>Home</Link>
+            </div>
+            <div class="w3-col s3">
+                <a href="#offers" class="w3-button w3-block" style = {styles.linkStyles}>Current Offers</a>
+            </div>
+            <div class="w3-col s3">
+                <a href="#makeoffer" class="w3-button w3-block" style = {styles.linkStyles}>Make an Offer</a>
+            </div>
+            <div class="w3-col s3">
+                <a href="#login" class="w3-button w3-git block" style = {styles.linkStyles}>Log In or Register</a>
+            </div>
+        </div>
+    </div>
+    
+  );
+}
+
+var styles = {
+  linkStyles: {
+    color: "#fff",
+    fontFamily: '"Lato", sans-serif',
+
+  },
+
+  navBar: {
+    backgroundColor: "black",
+    color: "#fff",
+    display: "flex",
+    justifyContent: "space-between",
+    padding: 20,
+}
+
+
+}
